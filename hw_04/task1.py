1a3addec579f808c51202b8f4975de1b7d627f1e
iluharider <iluharider2002@gmail.com>
2024-04-02 22:16:49 +0300

    task1

--- hw_04/task1.py
+++ hw_04/task1.py
@@ -1,54 +1,49 @@
-from time import perf_counter
+import time
+import threading
+import multiprocessing
+N = 38
+ITERATIONS = 10
 
-def fib(n: int):
-    return fib(n - 1) + fib(n - 2) if n > 1 else n
-
-
-def sync_fib(n: int, iters: int) -> float:
-    st = perf_counter()
-
-    for _ in range(iters):
+def fib(n):
+    if (n <= 1):
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+    
+def synchro(n, iters):
+    for i in range(iters):
         fib(n)
 
-    return perf_counter() - st
-
-
-def threading_fib(n: int, iters: int) -> float:
-    import threading
-    threads = [threading.Thread(target=fib, args=(n, )) for _ in range(iters)]
-
-    st = perf_counter()
-
+def threads(n, iters):
+    threads = [threading.Thread(target=fib, args=(n, )) for i in range(iters)]
     for thread in threads:
         thread.start()
 
     for thread in threads:
         thread.join()
 
-    return perf_counter() - st
+def processing(n, iters):
+    processes = [multiprocessing.Process(target=fib, args=(n, )) for i in range(iters)]
+    for process in processes:
+        process.start()
 
-
-def multiproc_fib(n: int, iters: int) -> float:
-    import multiprocessing
-    procs = [multiprocessing.Process(target=fib, args=(n, ))
-             for _ in range(iters)]
-
-    st = perf_counter()
-
-    for p in procs:
-        p.start()
-
-    for p in procs:
-        p.join()
-
-    return perf_counter() - st
-
+    for process in processes:
+        process.join()
 
 if __name__ == "__main__":
-    sync_time = sync_fib(36, 10)
-    thread_time = threading_fib(36, 10)
-    proc_time = multiproc_fib(36, 10)
-    with open("artifacts/task1.txt", 'w') as artifact:
-        artifact.write("sync: {}\n".format(sync_time))
-        artifact.write("threading: {}\n".format(thread_time))
-        artifact.write("multiprocessing: {}\n".format(proc_time))
+    start_sync = time.time()
+    synchro(N, ITERATIONS)
+    finish_sync = time.time()
+
+    start_thr = time.time()
+    threads_time = threads(N, ITERATIONS)
+    finish_thr = time.time()
+
+    start_prc = time.time()
+    processing_time = processing(N, ITERATIONS)
+    finish_prc = time.time()
+
+    with open("artifacts/task_1.txt", 'w') as file:
+        file.write(f"sync: {finish_sync - start_sync}\n")
+        file.write(f"threads: {finish_thr - start_thr}\n")
+        file.write(f"multiprocess: {finish_prc - start_prc}\n")
